4d995575eefbc2dbcd47da9ef3a3203adfcaf482
cgi <christian.gintenreiter@smarter-ecommerce.com>
2025-09-02 16:31:48 +0200

    joke_funnyness_factor_0_to_10

--- src/simplest/simplest_dspy.py
+++ src/simplest/simplest_dspy.py
@@ -8,7 +8,7 @@ def joke_for_john() -> str:
     the_joke_prediction = joker(name="John")
     return the_joke_prediction.joke
 
-def joke_funnyness_factor(joke: str) -> int:
+def joke_funnyness_factor_0_to_10(joke: str) -> int:
     funnyness_evaluator = dspy.Predict("joke -> funnyness_0_to_10: int")
     funnyness_prediction = funnyness_evaluator(joke=joke)
     return funnyness_prediction.funnyness_0_to_10
@@ -19,7 +19,7 @@ def main():
     the_joke: str = joke_for_john()
     print(f"\n\n{the_joke}")
     
-    funnyness: int = joke_funnyness_factor(the_joke)
+    funnyness: int = joke_funnyness_factor_0_to_10(the_joke)
     print(f" -> How funny is the joke on a scale of 0 to 10? {funnyness}\n")
 
 if __name__ == "__main__":
