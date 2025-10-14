92f6ed6876dab6f20d589002b561b0aeda6a0b07
cgi <christian.gintenreiter@smarter-ecommerce.com>
2025-10-14 15:51:22 +0200

    add max_tokens, temperature

--- src/common/utils.py
+++ src/common/utils.py
@@ -14,10 +14,11 @@ def dspy_configure(lm: dspy.LM, track_usage: bool = True):
     dspy.settings.configure(lm=lm, track_usage=track_usage)
     dspy.configure_cache(enable_disk_cache=False, enable_memory_cache=False)
 
-def get_lm_for_model_name(model_name: str, reasoning_effort: Literal["low", "medium", "high", "disable"] = "disable") -> dspy.LM:
+def get_lm_for_model_name(model_name: str, reasoning_effort: Literal["low", "medium", "high", "disable"] = "disable", max_tokens: int = 8192, temperature: float = 0.3) -> dspy.LM:
     model_access_prefix: str = get_model_access_prefix_or_fail()
     return dspy.LM(
         model=f'{model_access_prefix}{model_name}',
+        max_tokens=max_tokens, temperature=temperature,
         reasoning_effort=reasoning_effort # other options are Literal["low", "medium", "high"]
         # thinking={"type": "enabled", "budget_tokens": 512}
     )
